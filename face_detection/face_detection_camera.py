6ee49044b62b9938f010d36a5f983965d5b640a0
root <root@LAPTOP-2Q577UFQ>
2024-04-03 19:40:44 +0800

    face_detection

---
+++ face_detection/face_detection_camera.py
@@ -0,0 +1,32 @@
+# Import OpenCV
+import cv2
+import matplotlib.pyplot as plt
+
+# Create detector
+face_detector = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+
+# Video capture
+video_capture = cv2.VideoCapture(0)
+''' 0 denotes own webcam, 1 indicates some other webcam'''
+
+
+# Capture frame-by-frame
+while True:
+    ret, frame = video_capture.read()
+    image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detections = face_detector.detectMultiScale(image_gray)
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in detections:
+        print(w,h)
+        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+    
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done release the capture
+video_capture.release()
+cv2.destroyAllWindows()
